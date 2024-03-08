66befd9fcf97a92a88c7aae128645e920b72648d
♦️ 𝒩𝑒𝓍𝓊𝓈 𝓈𝑜𝒸𝒾𝑒𝓉𝓎 ♦️ <162390580+NexusSociety@users.noreply.github.com>
2024-03-07 22:14:22 -0300

    Update banner.py

--- banner.py
+++ banner.py
@@ -2,13 +2,17 @@ import pyfiglet
 
 
 def show_banner():
+    # Banner da aplicação
+    banner = ''' '''
+    
     disclaimer = '''
     [ Este software explora uma vulnerabilidade conhecida como SQL Injection no site "BANCO DA COREIA DO NORTE". ]
     [ O site em questão pertence á empresa Solyd, uma empresa voltada para a area de cibersegurnaça. ]
     [ Existe a permissão previa da empresa para a exploração de vulnerabilidades no aplicativo web! ]
     '''
 
-    banner = pyfiglet.Figlet('cosmic')
-    print(banner.renderText('Thief oF'))
-    print(banner.renderText('Korean'))
+    # banner = pyfiglet.Figlet('cosmic')
+    # print(banner.renderText('Thief oF'))
+    # print(banner.renderText('Korean'))
+    print(banner)
     print(disclaimer)
